f0d7bdd6209ffbc6c6322d2530fe5f03c877bfe8
Sean Outram <outramsean0@gmail.com>
2022-08-31 20:23:40 +0000

    Code upload for help

---
+++ pages/index.tsx
@@ -0,0 +1,9 @@
+import Layout from "../components/layout"
+
+export default function HomePage() {
+    return (
+        <Layout title="Home">
+            <div>Welcome to Next.js!</div>
+        </Layout>
+    );
+  }
